437c20345dd79958939067df04299614aaf1a3f8
Teodor Oprescu <T.Oprescu@student.tudelft.nl>
2024-06-14 02:25:41 +0200

    standardize the 5 types of experiments

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import wandb
 import separate_classifiers
 import divergent_heads_classifier
 import combined_classifier
+import freeze_training_disease_first
+import freeze_training_severity_first
 from torchvision import transforms
 from types import SimpleNamespace
 
 
 EXPERIMENT1 = SimpleNamespace(
-    classifier = "divergent_heads",
+    type = "divergent_heads",
     transforms = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
@@ -20,8 +22,9 @@ EXPERIMENT1 = SimpleNamespace(
     epochs = 25
 )
 
+
 EXPERIMENT2 = SimpleNamespace(
-    classifier = "combined",
+    type = "combined",
     transforms = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
@@ -32,6 +35,7 @@ EXPERIMENT2 = SimpleNamespace(
     project_name = "Teo Test Runs",
     epochs = 25
 )
+
 
 EXPERIMENTS = [
     EXPERIMENT1,
@@ -41,16 +45,24 @@ EXPERIMENTS = [
 
 def run_experiments(experiments):
     for experiment in experiments:
-        if experiment.classifier == "separate":
-            separate_classifiers.run_experiment(experiment)
-        elif experiment.classifier == "combined":
-            combined_classifier.run_experiment(experiment)
-        elif experiment.classifier == "divergent_heads":
-            divergent_heads_classifier.run_experiment(experiment)
-        else:
-            print("Invalid classifier type")
-            sys.exit(1)
-
+        try:
+            if experiment.type == "separate":
+                separate_classifiers.run_experiment(experiment)
+            elif experiment.type == "combined":
+                combined_classifier.run_experiment(experiment)
+            elif experiment.type == "divergent_heads":
+                divergent_heads_classifier.run_experiment(experiment)
+            elif experiment.type == 'freeze_disease':
+                freeze_training_disease_first.run_experiment(experiment)
+            elif experiment.type == 'freeze_severity':
+                freeze_training_severity_first.run_experiment(experiment)
+            else:
+                print("Invalid classifier type")
+                sys.exit(1)
+            print("Experiment completed: ", str(experiment))
+        except Exception as e:
+            print(f"Error running experiment: {e}")
+            continue
 
 if __name__ == '__main__':
     wandb.login(key="a8acb651e87c4dca872eeb0bdedcfccf93ab7171")
